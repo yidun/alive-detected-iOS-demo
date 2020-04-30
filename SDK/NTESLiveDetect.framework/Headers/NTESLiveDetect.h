2aeeff12648cac703d36d750929d7e2964ece0da
luolihao <luolihao@corp.netease.com>
2020-04-30 10:59:28 +0800

    增加sdk目录

---
+++ SDK/NTESLiveDetect.framework/Headers/NTESLiveDetect.h
@@ -0,0 +1,19 @@
+//
+//  NTESLiveDetect.h
+//  NTESLiveDetect
+//
+//  Created by Ke Xu on 2019/2/26.
+//  Copyright © 2019 Ke Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NTESLiveDetect.
+FOUNDATION_EXPORT double NTESLiveDetectVersionNumber;
+
+//! Project version string for NTESLiveDetect.
+FOUNDATION_EXPORT const unsigned char NTESLiveDetectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTESLiveDetect/PublicHeader.h>
+
+#import <NTESLiveDetect/NTESLiveDetectManager.h>
